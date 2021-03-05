4ade257f3f15f9cce6851395fe6ecaddc653107f
Timittra Islam Hridi <timittra.cse.aust@gmail.com>
2021-03-06 00:58:41 +0600

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart; 
+    // console.log(cart);
     // const total = cart.reduce( (total, prd) => total + prd.price, 0);
     let total=0; 
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price; 
+        total = total + product.price * product.quantity; 
     }
 
     let shipping = 0; 
@@ -29,12 +31,19 @@ const Cart = (props) => {
     }
     return (
         <div>
-            <h4>Order Summary</h4>
+            <h4 className="text-primary">Order Summary</h4>
             <p>Items ordered: {cart.length}</p>
             <p>Product Price: ${formatNumber(total)}</p>
             <p><small>Shipping Cost: ${shipping}</small></p>
             <p><small>Tax + VAT: ${formatNumber(tax)}</small></p>
             <p>Total Price: ${grandTotal}</p>
+            <br/>
+            {/* <Link to="/review">
+            <button className="main-button">Review Order</button>
+            </Link> */}
+            {
+                props.children
+            }
         </div>
     );
 };
